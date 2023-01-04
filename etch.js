d2ad5f131878cf020d93be6370f94857c314bef4
Becky Nasty <99774559+aurelius250@users.noreply.github.com>
2023-01-04 15:19:02 -0800

    Add two buttons and resize

--- etch.js
+++ etch.js
@@ -1,10 +1,20 @@
 let gridContainer = document.querySelector("#gridContainer"); 
 gridContainer.textContent= "bananas";
 
-function gridCreate(dimensions){ 
+function blackGradations(percent){ 
+    const rgbValue = 255 - (percent * 2.5);
+    return `rgb(${rgbValue}, ${rgbValue}, ${rgbValue})`
+}
+function randomizeRGB(){ 
+    const rgbValueOne = Math.floor(Math.random() * 255);
+    const rgbValueTwo = Math.floor(Math.random() * 255);
+    const rgbValueThree = Math.floor(Math.random() * 255);
+    const rgbValueFinal =  `rgb(${rgbValueOne}, ${rgbValueTwo}, ${rgbValueThree})`;
+    return rgbValueFinal;
+}
+function gridCreate(dimensions, type){ 
     document.getElementById("gridContainer").innerHTML="";
     paddingPerSquare = ((600 * 1.0)/ dimensions); 
-
     for(let i = 0; i < dimensions; i++){ 
         const rowGridContainer = document.createElement("div"); 
         rowGridContainer.classList = "gridRowContainer"; 
@@ -16,14 +26,30 @@ function gridCreate(dimensions){
             square.style.width = paddingPerSquare + "px"; 
             square.style.height = paddingPerSquare + "px";
             rowGridContainer.appendChild(square); 
-            square.addEventListener("mouseover", (e) => { 
-                square.style.background = "black";
+            const gradationTracker = {colorPercent: 0}; 
+            
+            square.addEventListener("mouseover", (e) =>  { 
+                if(type == "randomColorsMode") { 
+                square.style.backgroundColor = randomizeRGB();
+                return;
+                }
+                else if(type == "blackGradationsMode") { 
+                    if(gradationTracker.colorPercent < 100){ 
+                    gradationTracker.colorPercent += 10;
+                    square.style.backgroundColor = blackGradations(gradationTracker.colorPercent); 
+                    } else { 
+                    return; 
+                    }
+                } else if (type == "black") {
+                    square.style.backgroundColor = "black"; 
+                    return;
+                }
             })
         }
     }
 }
 
-function dimensionsPrompt(){ 
+function dimensionsPrompt(type){ 
     let gridSize = prompt("Please enter a grid size under 100");
     if(gridSize === "" ||  gridSize === null){ 
         return;
@@ -33,7 +59,7 @@ function dimensionsPrompt(){
         dimensionsPrompt();
     } 
     if(gridSize) { 
-        return gridCreate(gridSize);
+        return gridCreate(gridSize, type);
     } 
     else { 
         return;
@@ -43,7 +69,21 @@ function dimensionsPrompt(){
 //Execute this when button clicked
 const gridSizeButton = document.querySelector("#gridSizeButton");
 gridSizeButton.addEventListener("click", (e) => { 
-    dimensionsPrompt();
+    let type = "black";
+    dimensionsPrompt(type);
 })
 
-gridCreate(40);
+const randomColorsButton = document.querySelector("#randomColorsButton"); 
+randomColorsButton.addEventListener("click", () => { 
+    const type = "randomColorsMode";
+    dimensionsPrompt(type);
+})
+
+const blackGradationsButton = document.querySelector("#blackGradationsButton"); 
+blackGradationsButton.addEventListener("click", () => { 
+    const type = "blackGradationsMode";
+    dimensionsPrompt(type);
+})
+
+console.log(randomizeRGB());
+gridCreate(50, "black");
