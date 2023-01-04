c16b8229e9625a8e2a04d2833e5167d6d3f5be77
Becky Nasty <99774559+aurelius250@users.noreply.github.com>
2023-01-04 11:17:02 -0800

    Add variable inptu to grid size and implement btn

--- etch.js
+++ etch.js
@@ -1,20 +1,49 @@
 let gridContainer = document.querySelector("#gridContainer"); 
 gridContainer.textContent= "bananas";
 
-function gridSize(dimensions){ 
-    paddingPerSquare = 960 / dimensions; 
-    
-for(let i = 0; i < 16; i++){ 
+function gridCreate(dimensions){ 
+    document.getElementById("gridContainer").innerHTML="";
+    paddingPerSquare = (480 / dimensions); 
+for(let i = 0; i < dimensions; i++){ 
     const rowGridContainer = document.createElement("div"); 
     rowGridContainer.classList = "gridRowContainer"; 
     gridContainer.append(rowGridContainer);
-    for(let i = 0; i < 16; i++){ 
+    for(let i = 0; i < dimensions; i++){ 
         const square = document.createElement("div");
         square.classList = "squareInGrid";
-        rowGridContainer.appendChild(square);  
+        square.style.padding = paddingPerSquare + "px"; 
+        rowGridContainer.appendChild(square); 
         square.addEventListener("mouseover", (e) => { 
             square.style.background = "blue";
         })
     }
 }
 }
+
+function dimensionsPrompt(){ 
+    return prompt("Please enter a grid size under 100");
+}
+
+//Execute this when button clicked
+const gridSizeButton = document.querySelector("#gridSizeButton");
+gridSizeButton.addEventListener("click", (e) => { 
+    let gridSize = dimensionsPrompt();
+    let continueCondition = true;
+
+    while(continueCondition){ 
+    if(gridSize === ""){ 
+        alert("You have entered an empty value. Please enter a value betwen 1 and 100."); 
+        gridSize = dimensionsPrompt();
+    }
+    if(gridSize > 100 || gridSize < 1){ 
+        alert("Please enter a gridSize over 1 and under 100");
+        gridSize = dimensionsPrompt();
+    } else { 
+    gridCreate(gridSize);
+    continueCondition = false;
+    }
+}
+
+})
+
+
