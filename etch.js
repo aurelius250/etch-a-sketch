eaf8e1b699a6714abbb6cc38c48f2485de6cb0bf
Becky Nasty <99774559+aurelius250@users.noreply.github.com>
2023-01-04 12:51:24 -0800

    Finish exercise, add backgrounds, downsize canvas

--- etch.js
+++ etch.js
@@ -3,47 +3,47 @@ gridContainer.textContent= "bananas";
 
 function gridCreate(dimensions){ 
     document.getElementById("gridContainer").innerHTML="";
-    paddingPerSquare = (480 / dimensions); 
-for(let i = 0; i < dimensions; i++){ 
-    const rowGridContainer = document.createElement("div"); 
-    rowGridContainer.classList = "gridRowContainer"; 
-    gridContainer.append(rowGridContainer);
+    paddingPerSquare = ((600 * 1.0)/ dimensions); 
+
     for(let i = 0; i < dimensions; i++){ 
-        const square = document.createElement("div");
-        square.classList = "squareInGrid";
-        square.style.padding = paddingPerSquare + "px"; 
-        rowGridContainer.appendChild(square); 
-        square.addEventListener("mouseover", (e) => { 
-            square.style.background = "blue";
-        })
+        const rowGridContainer = document.createElement("div"); 
+        rowGridContainer.classList = "gridRowContainer"; 
+        gridContainer.append(rowGridContainer);
+
+        for(let i = 0; i < dimensions; i++){ 
+            const square = document.createElement("div");
+            square.classList = "squareInGrid";
+            square.style.width = paddingPerSquare + "px"; 
+            square.style.height = paddingPerSquare + "px";
+            rowGridContainer.appendChild(square); 
+            square.addEventListener("mouseover", (e) => { 
+                square.style.background = "black";
+            })
+        }
     }
-}
 }
 
 function dimensionsPrompt(){ 
-    return prompt("Please enter a grid size under 100");
+    let gridSize = prompt("Please enter a grid size under 100");
+    if(gridSize === "" ||  gridSize === null){ 
+        return;
+    }
+    if(gridSize > 100 || gridSize < 1){ 
+        alert("You have entered an invalid value. Please enter a value over 1 and under 100");
+        dimensionsPrompt();
+    } 
+    if(gridSize) { 
+        return gridCreate(gridSize);
+    } 
+    else { 
+        return;
+    }
 }
 
 //Execute this when button clicked
 const gridSizeButton = document.querySelector("#gridSizeButton");
 gridSizeButton.addEventListener("click", (e) => { 
-    let gridSize = dimensionsPrompt();
-    let continueCondition = true;
-
-    while(continueCondition){ 
-    if(gridSize === ""){ 
-        alert("You have entered an empty value. Please enter a value betwen 1 and 100."); 
-        gridSize = dimensionsPrompt();
-    }
-    if(gridSize > 100 || gridSize < 1){ 
-        alert("Please enter a gridSize over 1 and under 100");
-        gridSize = dimensionsPrompt();
-    } else { 
-    gridCreate(gridSize);
-    continueCondition = false;
-    }
-}
-
+    dimensionsPrompt();
 })
 
-
+gridCreate(40);
